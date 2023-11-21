eb83254c70876b4691ee77599adc31f3206b76be
phamty <114122026+Phamty2002@users.noreply.github.com>
2023-11-22 00:14:46 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, ThemeContext } from './context/ThemeContext';
 
+import './styles/styles.css'
 // Importing existing components
 import SignIn from './components/Authentication/Login';
 import SignUp from './components/Authentication/Signup';
@@ -21,35 +23,52 @@ import ManageOrder from './components/Order/ManageOrder-emp';
 import GetOrderUser from './components/Order/GetOrder-users';
 import { ProductsProvider } from './context/ProductsContext';
 
-function App() {
+
+
+function AppWrapper() {
   return (
-     
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+}
+
+function App() {
+  const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    // Apply the theme class to the body element
+    document.body.className = theme === 'dark' ? 'dark-mode' : '';
+  }, [theme]);
+
+  return (
+    
       <Router>
         <ProductsProvider>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/home-emp" element={<HomeEmp />} />
-          <Route path="/home-user" element={<HomeUser />} />
-          <Route path="/menu-user" element={<MenuUser />} />
-          <Route path="/menu-emp" element={<MenuEmp />} />
-          <Route path="/crud" element={<CrudProduct />} />
-          <Route path="/about" element={<Aboutus />} />
-          <Route path="/profile" element={<Profile />} />
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/home-emp" element={<HomeEmp />} />
+            <Route path="/home-user" element={<HomeUser />} />
+            <Route path="/menu-user" element={<MenuUser />} />
+            <Route path="/menu-emp" element={<MenuEmp />} />
+            <Route path="/crud" element={<CrudProduct />} />
+            <Route path="/about" element={<Aboutus />} />
+            <Route path="/profile" element={<Profile />} />
 
-          {/* Routes for order management */}
-          <Route path="/place-order" element={<PlaceOrder />} />
-          <Route path="/get-order" element={<GetOrder />} />
-          <Route path="/get-order-users" element={<GetOrderUser />} />
-          <Route path="/update-order" element={<UpdateOrder />} />
-          <Route path="/delete-order" element={<DeleteOrder />} />
-          <Route path="/orders-emp" element={<ManageOrder />} />
-        </Routes>
+            {/* Routes for order management */}
+            <Route path="/place-order" element={<PlaceOrder />} />
+            <Route path="/get-order" element={<GetOrder />} />
+            <Route path="/get-order-users" element={<GetOrderUser />} />
+            <Route path="/update-order" element={<UpdateOrder />} />
+            <Route path="/delete-order" element={<DeleteOrder />} />
+            <Route path="/orders-emp" element={<ManageOrder />} />
+          </Routes>
         </ProductsProvider>
       </Router>
     
   );
 }
 
-export default App;
+export default AppWrapper;
--- src/components/Header/SideBar.js
+++ src/components/Header/SideBar.js
@@ -1,36 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './SideBar.css';
+import { ThemeContext } from '../../context/ThemeContext'; // Make sure the path is correct
 
 function Dropdown({ label, children }) {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <li>
-            <button onClick={() => setIsOpen(!isOpen)}>{label}</button>
+        <li className="dropdown">
+            <button onClick={() => setIsOpen(!isOpen)}>
+                {label}
+                <span className="dropdown-icon">{isOpen ? '▲' : '▼'}</span>
+            </button>
             {isOpen && <ul>{children}</ul>}
         </li>
     );
 }
 
 function Sidebar() {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
     return (
-        <div className="sidebar">
+        <div className={`sidebar ${theme === 'dark' ? 'dark-mode' : ''}`}>
             <h2>Dashboard</h2>
             <ul>
                 <li><Link to="/home-emp">Home</Link></li>
                 <li><Link to="/profile">Users Management</Link></li>
                 <li><Link to="/crud">Products Management</Link></li>
                 <Dropdown label="Orders Management">
-                <li><Link to="/place-order">Add Order</Link></li>
-                <li><Link to="/get-order">Retrieve Order</Link></li>
-                <li><Link to="/update-order">Update Order</Link></li>
-                <li><Link to="/delete-order">Delete Order</Link></li>
+                    <Link to="/place-order">Add Order</Link>
+                    <Link to="/get-order">Retrieve Order</Link>
+                    <Link to="/update-order">Update Order</Link>
+                    <Link to="/delete-order">Delete Order</Link>
                 </Dropdown>
                 <li><Link to="">Settings</Link></li>
                 <li><Link to="/sign-in"><strong>Sign Out</strong></Link></li>
-                
             </ul>
+            <li>
+                <button onClick={toggleTheme}>Dark Mode</button>
+            </li>
         </div>
     );
 }
